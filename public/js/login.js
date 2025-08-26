db9f069ee3816b458607f1709ded40bdb092ffc3
adolfosa <adolfo.sepulveda.astroza@gmail.com>
2025-08-26 12:13:39 -0400

    fix url forgot pass

--- public/js/login.js
+++ public/js/login.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const email = document.getElementById('emailRecuperar').value;
       
       try {
-        const response = await fetch('http://localhost:4000/api/auth/forgot', {
+        const response = await fetch('https://localhost:4000/api/auth/forgot', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email })
